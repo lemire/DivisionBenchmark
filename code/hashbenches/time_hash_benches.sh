099620d32ccb68a0af67e5994cfb930705261a51
Daniel Lemire <lemire@gmail.com>
2018-10-12 13:22:51 -0400

    Another missing script.

---
+++ code/hashbenches/time_hash_benches.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+TIMEFORMAT=%R
+PERFCMD="time" # could be perf stat
+CCSTRINGS=("cc -march=native -O3" )
+
+
+for CCSTRING in "${CCSTRINGS[@]}" ; do
+    echo "#" Machine `hostname`,  compiling with $CCSTRING
+    CC_SEE_ASM="$CCSTRING -S"  #if multiple compilers, keep the last one
+    echo '#*********************** UNSIGNED ******************************'
+    echo "# divisor  lkk justdivlkk $LIBDIV_OPTS system slow"
+
+    END=64 #4096
+
+    for ((i=2;i<=END;i++)); do
+      s=$i
+      outputs=()
+      for opt in lkk justdivlkk $LIBDIV_OPTS system slow ; do
+          touch a.out
+          rm a.out
+          $CCSTRING   hash_benchmark_${opt}.c -DD=$i
+          $CC_SEE_ASM hash_benchmark_${opt}.c -DD=$i
+          thisresult=$( { time ./a.out; } 2>&1 )
+          outputs+=($?)
+          s+=" "$thisresult
+      done
+      refout=${outputs[0]}
+      for oute in "${outputs[@]}"
+      do
+          if [ "$refout" != "$oute" ]
+          then
+            echo "bug? ${outputs[@]}"
+            exit -1
+          fi
+      done
+      echo $s
+    done
+
+    BIGEND=2000000000
+    for ((i=END+1;i<=BIGEND;i=i/10*19)); do
+      s=$i
+      outputs=()
+      for opt in lkk justdivlkk $LIBDIV_OPTS system slow ; do
+          touch a.out
+          rm a.out
+          $CCSTRING   hash_benchmark_${opt}.c -DD=$i
+          $CC_SEE_ASM hash_benchmark_${opt}.c -DD=$i
+          thisresult=$( { time ./a.out; } 2>&1 )
+          outputs+=($?)
+          s+=" "$thisresult
+      done
+      refout=${outputs[0]}
+      for oute in "${outputs[@]}"
+      do
+          if [ "$refout" != "$oute" ]
+          then
+            echo "bug? ${outputs[@]}"
+            exit -1
+          fi
+      done
+      echo $s
+    done
+
+    echo
+    echo
+done
