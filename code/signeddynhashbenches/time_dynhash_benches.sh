e0dcbb8ab791eeb6535ccd1d1cb9c0bb2d4a2f97
Daniel Lemire <lemire@gmail.com>
2018-10-12 13:22:30 -0400

    Missing scripts.

---
+++ code/signeddynhashbenches/time_dynhash_benches.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+TIMEFORMAT=%R
+PERFCMD="time" # could be perf stat
+CCSTRINGS=("cc -march=native  -O3" )
+
+for CCSTRING in "${CCSTRINGS[@]}" ; do
+    echo "#" Machine `hostname`,  compiling with $CCSTRING
+    CC_SEE_ASM="$CCSTRING -S"  #if multiple compilers, keep the last one
+    echo '#*********************** SIGNED ******************************'
+    echo "# divisor  lkk justdivlkk $LIBDIV_OPTS system "
+
+    END=4096
+    for opt in lkk justdivlkk $LIBDIV_OPTS system  ; do
+       $CCSTRING  -o dynhash_benchmark_${opt}  dynhash_benchmark_${opt}.c
+       $CC_SEE_ASM  dynhash_benchmark_${opt}.c
+    done
+    for ((i=2;i<=END;i++)); do
+      s=$i
+      outputs=()
+      for opt in lkk justdivlkk $LIBDIV_OPTS system  ; do
+         thisresult=$( { time ./dynhash_benchmark_${opt} $i; } 2>&1 )
+          outputs+=($?)
+          s+=" "$thisresult
+      done
+      refout=${outputs[0]}
+      for oute in "${outputs[@]}"
+      do
+          if [ "$refout" != "$oute" ]
+          then
+            echo "bug? ${outputs[@]}"
+            exit -1
+          fi
+      done
+      echo $s
+    done
+    echo
+    echo
+done
---
+++ code/signedhashbenches/time_hash_benches.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+TIMEFORMAT=%R
+PERFCMD="time" # could be perf stat
+CCSTRINGS=("cc -march=native  -O3" )
+
+for CCSTRING in "${CCSTRINGS[@]}" ; do
+    echo "#" Machine `hostname`,  compiling with $CCSTRING
+    CC_SEE_ASM="$CCSTRING -S"  #if multiple compilers, keep the last one
+    echo '#*********************** SIGNED ******************************'
+    echo "# divisor  lkk justdivlkk $LIBDIV_OPTS system slow"
+
+    END=4096
+
+    for ((i=2;i<=END;i++)); do
+      s=$i
+      outputs=()
+      for opt in lkk justdivlkk $LIBDIV_OPTS system slow ; do
+          touch a.out
+          rm a.out
+          $CCSTRING   hash_benchmark_${opt}.c -DD=$i
+          $CC_SEE_ASM hash_benchmark_${opt}.c -DD=$i
+          thisresult=$( { time ./a.out; } 2>&1 )
+          outputs+=($?)
+          s+=" "$thisresult
+      done
+      refout=${outputs[0]}
+      for oute in "${outputs[@]}"
+      do
+          if [ "$refout" != "$oute" ]
+          then
+            echo "bug? ${outputs[@]}"
+            exit -1
+          fi
+      done
+      echo $s
+    done
+    echo
+    echo
+done
