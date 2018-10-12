4091a1be8060aa031ca80699471425be03a3afc1
Daniel Lemire <lemire@gmail.com>
2018-10-12 13:20:55 -0400

    Enabling all....

--- runme.sh
+++ runme.sh
@@ -3,34 +3,34 @@ cc --version
 cd code
 
 echo "codesize"
-#cd codesize
-#make
-#cd .. 
+cd codesize
+make
+cd .. 
 
-#cd benches
-#echo "simplified_primes_benches.sh"
-#./simplified_primes_benches.sh
-#cd ..
+cd benches
+echo "simplified_primes_benches.sh"
+./simplified_primes_benches.sh
+cd ..
 
-#cd collatzbenches
-#echo "collatzbenches"
-#./timerun.sh
-#cd ..
+cd collatzbenches
+echo "collatzbenches"
+./timerun.sh
+cd ..
 
-#echo "hashbenches"
-#cd hashbenches
-#./time_hash_benches.sh
-#cd ..
+echo "hashbenches"
+cd hashbenches
+./time_hash_benches.sh
+cd ..
 
-#echo "dynhashbenches"
-#cd dynhashbenches
-#./time_dynhash_benches.sh
-#cd ..
+echo "dynhashbenches"
+cd dynhashbenches
+./time_dynhash_benches.sh
+cd ..
 
-#echo "signeddynhashbenches"
-#cd signeddynhashbenches
-#./time_dynhash_benches.sh
-#cd ..
+echo "signeddynhashbenches"
+cd signeddynhashbenches
+./time_dynhash_benches.sh
+cd ..
 
 
 echo "signedhashbenches"
@@ -38,4 +38,4 @@ cd signedhashbenches
 ./time_hash_benches.sh
 cd ..
 
-#for file in `find . -type f -name \*.s` ;  do echo $file; cat $file ; done
+for file in `find . -type f -name \*.s` ;  do echo $file; cat $file ; done
