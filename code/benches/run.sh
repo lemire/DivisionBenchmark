ef8befeca0ad775887dbec8bcb4c6a66357c5e88
Daniel Lemire <lemire@gmail.com>
2018-10-13 15:44:18 -0400

    Unneeded script.

--- code/benches/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-HOSTNAME=`hostname`
-LOGFILE=results/data-${HOSTNAME}.txt 
-mkdir -p results
-echo "************** Tests run `date` ********************" >>$LOGFILE
-./simplified_primes_benches.sh  >>$LOGFILE 2>&1
-
